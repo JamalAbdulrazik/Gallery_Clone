1748bf2700f70a1c396834a4d1eca2ae2af6d9a9
jamal <jamalabdulrazik@gmail.com>
2022-12-11 14:21:02 +0200

    Finished carousel blueprint and the categories expanded list + creating a simple app bar demo file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
-
-
-const _horizontalPadding = 32.0;
-
+import 'demos_viewer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,27 +8,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
         children: [
-          ListView(
-            key: const ValueKey('HomeListView'),
-            primary: true,
-            restorationId: 'home_list_view',
-            children: [
-              Container(
-                margin:
-                const EdgeInsets.symmetric(horizontal: _horizontalPadding),
-                child: _GalleryHeader(),
-              ),
-              Container(
-                margin:
-                const EdgeInsets.symmetric(horizontal: _horizontalPadding),
-                child: _CategoriesHeader(),
-              ),
-            ],
-          ),
+          _GalleryHeader(),
+          _Carousel(),
+          _CategoriesHeader(),
+          _MaterialCategoryItem(),
         ],
       ),
+    );
+  }
+}
+
+class MaterialItem {
+  MaterialItem(
+      {required this.title,
+        required this.subtitle,
+        required this.icon,
+        required this.widget});
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final Widget widget;
+}
+class _MaterialCategoryItem extends StatelessWidget {
+   _MaterialCategoryItem({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text('MATERIAL'),
+      leading: Icon(Icons.abc),
+      children: [
+        ListTile(
+          leading: const Icon(Icons.abc_outlined),
+          title: const Text('App Bar'),
+          subtitle: const Text(
+              'Displaying information and actions relating to the current screen'),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const DemosViewer()));
+          },
+        ),
+      ],
+    );
+  }
+}
+
+
+class _Carousel extends StatelessWidget {
+  const _Carousel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250.0,
+      child: ListView.builder(
+          itemCount: 2,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const _CarouselCard();
+          }),
+    );
+  }
+}
+
+class _CarouselCard extends StatelessWidget {
+  const _CarouselCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SizedBox(height: 200.0, child: Placeholder()),
     );
   }
 }
@@ -57,7 +112,8 @@ class _CategoriesHeader extends StatelessWidget {
 }
 
 class Header extends StatelessWidget {
-  const Header({Key? key, required this.color, required this.text}) : super(key: key);
+  const Header({super.key, required this.color, required this.text});
+
   final Color color;
   final String text;
 
@@ -66,14 +122,18 @@ class Header extends StatelessWidget {
     return Align(
       alignment: AlignmentDirectional.centerStart,
       child: Padding(
-        padding: const EdgeInsets.only(top:15.0, bottom: 11.0),
+        padding: EdgeInsets.only(
+          top: 15,
+          bottom: 11,
+        ),
         child: SelectableText(
           text,
-          style: Theme.of(context).textTheme.headlineMedium!.apply(color: color, fontSizeDelta: 0.0),
+          style: Theme.of(context).textTheme.headlineMedium!.apply(
+                color: color,
+                fontSizeDelta: 0,
+              ),
         ),
       ),
     );
   }
 }
-
-
